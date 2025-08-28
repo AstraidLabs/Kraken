175c8405239456bc89fffb6231ae0113113a9de4
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2025-08-28 12:04:14 +0200

    Přidejte soubory projektu.

---
+++ Kraken/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Kraken
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
